789c7c303cd669f9f1cc7e8a3fc2f122ec54af9b
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-19 20:17:12 +0100

    Create MathSqrt.sol

---
+++ contracts/math/MathSqrt.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+library MathSqrt {
+    function sqrt(uint256 y) public pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+}
