0e5495a192a0cd5309a9d78e10a43d03a627ec54
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-19 20:20:20 +0100

    Create MathLog2.sol

---
+++ contracts/math/MathLog2.sol
@@ -0,0 +1,10 @@
+library MathLog2 {
+    function log2(uint256 x) public pure returns (uint256) {
+        uint256 res = 0;
+        while (x > 1) {
+            x >>= 1;
+            res++;
+        }
+        return res;
+    }
+}
