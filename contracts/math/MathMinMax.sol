067d8a69f5dda2efda991bc5b2f7976fe5294e55
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-19 20:21:11 +0100

    Create MathMinMax.sol

---
+++ contracts/math/MathMinMax.sol
@@ -0,0 +1,4 @@
+library MathMinMax {
+    function min(uint256 a, uint256 b) public pure returns (uint256) { return a < b ? a : b; }
+    function max(uint256 a, uint256 b) public pure returns (uint256) { return a > b ? a : b; }
+}
