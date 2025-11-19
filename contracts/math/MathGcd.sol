6a34c77f48e66cb823bc4b08aa0c1f7b52945581
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-19 20:19:52 +0100

    Create MathGcd.sol

---
+++ contracts/math/MathGcd.sol
@@ -0,0 +1,10 @@
+library MathGcd {
+    function gcd(uint256 a, uint256 b) public pure returns (uint256) {
+        while (b != 0) {
+            uint256 t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+}
