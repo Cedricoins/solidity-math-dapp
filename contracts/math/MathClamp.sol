d032616babb5bf293469756d63bb47cdcaf909e8
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-19 20:21:22 +0100

    Create MathClamp.sol

---
+++ contracts/math/MathClamp.sol
@@ -0,0 +1,5 @@
+library MathClamp {
+    function clamp(uint256 value, uint256 minVal, uint256 maxVal) public pure returns (uint256) {
+        return value < minVal ? minVal : (value > maxVal ? maxVal : value);
+    }
+}
