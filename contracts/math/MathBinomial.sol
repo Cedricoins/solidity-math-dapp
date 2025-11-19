1b23d6b8784976f62541caa129a9954015dfdef1
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-19 20:22:30 +0100

    Create MathBinomial.sol

---
+++ contracts/math/MathBinomial.sol
@@ -0,0 +1,12 @@
+library MathBinomial {
+    function binomial(uint256 n, uint256 k) public pure returns (uint256) {
+        if (k > n) return 0;
+        if (k == 0 || k == n) return 1;
+        k = k > n - k ? n - k : k;
+        uint256 result = 1;
+        for (uint256 i = 1; i <= k; i++) {
+            result = result * (n - k + i) / i;
+        }
+        return result;
+    }
+}
