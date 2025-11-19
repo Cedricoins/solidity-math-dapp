15660ccfc5aa32387ff76b165627e14ce18994df
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-19 20:23:05 +0100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
