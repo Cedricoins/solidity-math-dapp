9bfe7f7f05a1747e921fc55e84f74165b09d5cca
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-19 20:20:32 +0100

    Create MathLog10.sol

---
+++ contracts/math/MathLog10.sol
@@ -0,0 +1,7 @@
+library MathLog10 {
+    uint256 constant LOG10_2 = 30102999566; // log10(2)*1e10 approx
+
+    function log10(uint256 x) public pure returns (uint256) {
+        return (MathLn.ln(x) * 1e10) / (4342944819); // ln(10)*1e10 approx
+    }
+}
