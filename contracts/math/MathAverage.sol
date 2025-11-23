568c5faf8c96f74f0381ea8115d9ae5a11b21150
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 17:57:25 +0100

    Update MathAverage.sol

--- contracts/math/MathAverage.sol
+++ contracts/math/MathAverage.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 library MathAverage {
     function avg(uint256 a, uint256 b) public pure returns (uint256) {
         return (a & b) + (a ^ b) / 2;
