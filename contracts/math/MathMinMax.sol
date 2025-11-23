efb505cbd494c1274ebd03df1978ca3ede9ea1cd
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 17:59:04 +0100

    Update MathMinMax.sol

--- contracts/math/MathMinMax.sol
+++ contracts/math/MathMinMax.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 library MathMinMax {
     function min(uint256 a, uint256 b) public pure returns (uint256) { return a < b ? a : b; }
     function max(uint256 a, uint256 b) public pure returns (uint256) { return a > b ? a : b; }
