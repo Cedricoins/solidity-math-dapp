889c5b962654e07bb4b574ed8cc32748e5a4ca25
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 17:58:37 +0100

    Update MathLcm.sol

--- contracts/math/MathLcm.sol
+++ contracts/math/MathLcm.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 library MathLcm {
     function lcm(uint256 a, uint256 b) public pure returns (uint256) {
         return a / MathGcd.gcd(a, b) * b;
