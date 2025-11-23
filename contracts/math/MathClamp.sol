faf247981df9ef2aaa5c356da66fd95e87fdf981
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 17:57:43 +0100

    Update MathClamp.sol

--- contracts/math/MathClamp.sol
+++ contracts/math/MathClamp.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 library MathClamp {
     function clamp(uint256 value, uint256 minVal, uint256 maxVal) public pure returns (uint256) {
         return value < minVal ? minVal : (value > maxVal ? maxVal : value);
