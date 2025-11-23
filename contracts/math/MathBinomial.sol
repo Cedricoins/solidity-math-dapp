bd8defedf986db22cba4898b646a383ea73b14ba
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 17:57:35 +0100

    Update MathBinomial.sol

--- contracts/math/MathBinomial.sol
+++ contracts/math/MathBinomial.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 library MathBinomial {
     function binomial(uint256 n, uint256 k) public pure returns (uint256) {
         if (k > n) return 0;
