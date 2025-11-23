7d3cfd2fb2eede1d6e6786737d64dd081cea28d6
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 17:57:11 +0100

    Update MathAbs.sol

--- contracts/math/MathAbs.sol
+++ contracts/math/MathAbs.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 library MathAbs {
     function abs(int256 x) public pure returns (uint256) {
         return x < 0 ? uint256(-x) : uint256(x);
