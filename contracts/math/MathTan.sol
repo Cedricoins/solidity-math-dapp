1c18da19e05b1cde31f1f52a2012d0fbca03d088
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 17:59:29 +0100

    Update MathTan.sol

--- contracts/math/MathTan.sol
+++ contracts/math/MathTan.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 library MathTan {
     function tan(int256 x) public pure returns (int256) {
         return MathSin.sin(x) / MathCos.cos(x);
