3a659ed0a42afb6096b14ef736e1f38e65992c8e
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 17:58:03 +0100

    Update MathFactorial.sol

--- contracts/math/MathFactorial.sol
+++ contracts/math/MathFactorial.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 library MathFactorial {
     function factorial(uint256 n) public pure returns (uint256) {
         require(n <= 20, "too big");
